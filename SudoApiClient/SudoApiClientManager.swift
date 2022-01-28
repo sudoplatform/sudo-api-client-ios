6b841d498bf6d17990729380eec8373b96b45776
Sudo Platform Engineering <sudoplatform-engineering@anonyome.com>
2022-01-28 11:27:36 +1000

    Release 8.0.0

--- SudoApiClient/SudoApiClientManager.swift
+++ SudoApiClient/SudoApiClientManager.swift
@@ -20,10 +20,10 @@ public class SudoApiClientManager {
 
     /// Serial operation queue shared (defaut) by `SudoApiClient` instances for GraphQL mutations and queries with unsatisfied
     /// preconditions.
-    public static let serialOperationQueue = ApiOperationQueue(maxConcurrentOperationCount: 1, maxQueueDepth: 10)
+    public static let serialOperationQueue: ApiOperationQueue = DefaultApiOperationQueue(maxConcurrentOperationCount: 1, maxQueueDepth: 10)
 
     /// Concurrent operation queue shaed (default) by `SudoApiClient` instances for GraphQL queries with all preconditions met.
-    public static let concurrentOperationQueue = ApiOperationQueue(maxConcurrentOperationCount: 3, maxQueueDepth: 10)
+    public static let concurrentOperationQueue: ApiOperationQueue = DefaultApiOperationQueue(maxConcurrentOperationCount: 3, maxQueueDepth: 10)
 
     private struct Config {
 
